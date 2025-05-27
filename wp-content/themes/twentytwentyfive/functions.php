8a344ae1bd1f6b3ed9c4d94f9f48899eff5bf4fd
DMALKOV\dmalkov <demoncfh@mail.ru>
2025-05-27 20:01:52 +0400

    фильтрация товаров

--- wp-content/themes/twentytwentyfive/functions.php
+++ wp-content/themes/twentytwentyfive/functions.php
@@ -157,6 +157,7 @@ if ( ! function_exists( 'twentytwentyfive_format_binding' ) ) :
 	}
 endif;
 
+/**  Регистрация REST API endpoint для получения атрибутов товаров категории */
 add_action('rest_api_init', function() {
     register_rest_route('custom/v1', '/category-attributes/(?P<id>\d+)', [
         'methods'  => 'GET',
@@ -168,13 +169,13 @@ add_action('rest_api_init', function() {
 function get_category_attributes($request) {
     $category_id = $request['id'];
     
-    // Get category term
+    // Получаем категорию
     $term = get_term($category_id, 'product_cat');
     if (!$term || is_wp_error($term)) {
         return [];
     }
 
-    // Get products using WP_Query
+    // Получаем все товары категории
     $args = array(
         'post_type' => 'product',
         'posts_per_page' => -1,
@@ -198,30 +199,164 @@ function get_category_attributes($request) {
         foreach ($product->get_attributes() as $attr) {
             $attr_id = $attr->is_taxonomy() ? sanitize_title($attr->get_name()) : $attr->get_id();
             
-            // Get options with their names
+            // Получаем опции атрибута
             $options = [];
             if ($attr->is_taxonomy()) {
                 $terms = $attr->get_terms();
                 if (!is_wp_error($terms)) {
                     foreach ($terms as $term) {
-                        $options[] = $term->name;
+                        $options[] = [
+                            'name' => $term->name,
+                            'slug' => $term->slug
+                        ];
                     }
                 }
             } else {
-                $options = $attr->get_options();
+                foreach ($attr->get_options() as $option) {
+                    $options[] = [
+                        'name' => $option,
+                        'slug' => sanitize_title($option)
+                    ];
+                }
             }
 
-            $attributes[$attr_id] = [
-                'id'      => $attr->get_id(),
-                'name'    => $attr->get_name(),
-                'title'   => wc_attribute_label($attr->get_name()),
-                'options' => array_unique(array_merge(
-                    $attributes[$attr_id]['options'] ?? [],
-                    $options
-                ))
-            ];
+            // Если атрибут уже есть в массиве, объединяем опции
+            if (isset($attributes[$attr_id])) {
+                $existing_options = $attributes[$attr_id]['options'];
+                $merged_options = array_merge($existing_options, $options);
+                $unique_options = array_values(array_unique($merged_options, SORT_REGULAR));
+                $attributes[$attr_id]['options'] = $unique_options;
+            } else {
+                $attributes[$attr_id] = [
+                    'id'      => $attr->get_id(),
+                    'name'    => $attr->get_name(),
+                    'title'   => wc_attribute_label($attr->get_name()),
+                    'slug'    => $attr->is_taxonomy() ? $attr->get_name() : sanitize_title($attr->get_name()),
+                    'options' => array_values(array_unique($options, SORT_REGULAR))
+                ];
+            }
         }
     }
 
-    return array_values($attributes);
+    // Сортируем атрибуты по названию (title)
+    usort($attributes, function($a, $b) {
+        return strcmp($a['title'], $b['title']);
+    });
+
+    // Сортируем опции каждого атрибута по названию (name)
+    foreach ($attributes as &$attribute) {
+        usort($attribute['options'], function($a, $b) {
+            return strcmp($a['name'], $b['name']);
+        });
+    }
+
+    return $attributes;
+}
+
+/**
+ * Поиск товаров по нескольким фильтрам и атрибутам одного типа.
+ *
+ * @param array $filters Массив фильтров (например, категория, цена и т.д.).
+ * @param array $attributes Массив атрибутов для фильтрации.
+ * @return WP_Query Результаты поиска товаров.
+ */
+function search_products_by_filters_and_attributes($filters = array(), $attributes = array()) {
+    $args = array(
+        'post_type'      => 'product',
+        'posts_per_page' => -1,
+        'post_status'    => 'publish',
+        'tax_query'      => array(), // Инициализируем tax_query
+        'meta_query'     => array(), // Инициализируем meta_query
+    );
+
+    // Добавляем фильтр по категории
+    if (!empty($filters) && isset($filters['category_id'])) {
+        $args['tax_query'][] = array(
+            'taxonomy' => 'product_cat',
+            'field'    => 'term_id',
+            'terms'    => (int) $filters['category_id'], // Убедимся, что term_id является числом
+            'operator' => 'IN', // Используем 'IN' для совместимости
+        );
+    }
+
+    // Добавляем фильтрацию по цене
+    if (!empty($filters) && (isset($filters['min_price']) || isset($filters['max_price']))) {
+        $min_price = isset($filters['min_price']) ? (float) $filters['min_price'] : 0;
+        $max_price = isset($filters['max_price']) ? (float) $filters['max_price'] : PHP_FLOAT_MAX;
+
+        $args['meta_query'][] = array(
+            'key'     => '_price',
+            'value'   => array($min_price, $max_price),
+            'compare' => 'BETWEEN',
+            'type'    => 'NUMERIC',
+        );
+    }
+
+    // Добавляем фильтрацию по атрибутам (логическое ИЛИ)
+    if (!empty($attributes)) {
+        foreach ($attributes as $attr_name => $attr_values) {
+            $args['tax_query'][] = array(
+                'taxonomy' => $attr_name,
+                'field'    => 'slug',
+                'terms'    => $attr_values,
+                'operator' => 'IN',
+            );
+        }
+    }
+
+    // Устанавливаем отношение 'AND' для tax_query, если есть несколько условий
+    if (count($args['tax_query']) > 1) {
+        $args['tax_query']['relation'] = 'AND';
+    }
+
+    // Логирование для отладки (можно удалить после проверки)
+    error_log('WP_Query args: ' . print_r($args, true));
+
+    $query = new WP_Query($args);
+
+    // Логирование результатов запроса
+    error_log('Found posts: ' . $query->found_posts);
+
+    return $query;
+}
+
+// Регистрация REST API endpoint для поиска товаров
+add_action('rest_api_init', function () {
+    register_rest_route('custom/v1', '/search-products', [
+        'methods'  => 'POST',
+        'callback' => 'handle_product_search_request',
+        'permission_callback' => '__return_true', // Разрешить доступ всем (можно настроить ограничения)
+    ]);
+});
+
+/**
+ * Обработчик запроса поиска товаров.
+ *
+ * @param WP_REST_Request $request Объект запроса.
+ * @return WP_REST_Response Ответ с результатами поиска.
+ */
+function handle_product_search_request(WP_REST_Request $request) {
+    $filters = $request->get_param('filters') ?: array();
+    $attributes = $request->get_param('attributes') ?: array();
+
+    $query = search_products_by_filters_and_attributes($filters, $attributes);
+
+    $products = array();
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $product_id = get_the_ID();
+            $product = wc_get_product($product_id);
+
+            $products[] = array(
+                'id'    => $product_id,
+                'title' => get_the_title(),
+                'price' => $product->get_price(),
+                'link'  => get_permalink(),
+            );
+        }
+        wp_reset_postdata();
+    }
+
+    return new WP_REST_Response($products, 200);
 }
